3d7f8f470568f13d32986b77a2d79001c10b5e97
Diego Cabrera Nieto <30873930+Diegocn0000@users.noreply.github.com>
2018-03-05 21:06:24 -0600

    Version Principal

---
+++ Assets/Texture/Move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+    public GameObject player;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        player.transform.Translate(new Vector3(x,0,y)*Time.deltaTime*speed);
+    }
+}
