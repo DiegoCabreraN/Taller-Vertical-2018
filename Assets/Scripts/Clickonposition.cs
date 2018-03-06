e88f5003a66c03c1ce80cfe1a64485735d42d90e
Diego Cabrera Nieto <30873930+Diegocn0000@users.noreply.github.com>
2018-03-06 08:36:23 -0600

    Version modificada

---
+++ Assets/Scripts/Clickonposition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickonposition : MonoBehaviour {
+    public GameObject VR, pos;
+    public float speed;
+    public Vector3 position;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (VR.transform.position != pos.transform.position)
+        {
+            VR.transform.position = position;
+        }
+        else {
+            VR.transform.position = pos.transform.position;
+        }
+        
+	}
+    public void move() {
+        float x = pos.transform.position.x;
+        float z = pos.transform.position.z;
+        position = new Vector3(x, -0.6f, z);
+
+    }
+}
