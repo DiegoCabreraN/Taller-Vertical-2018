a17db4ef57a75459cef3029817a46953212e403a
Dennis Kingston <d.kingston12@gmail.com>
2018-03-08 18:24:17 -0600

    Botón para finalizar juego

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour {
+
+	public GameObject hand;
+	public bool isTouched = false;
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.name == "hand" && isTouched == false)
+		{
+			transform.localScale -= new Vector3(0.1F, 0, 0); 			
+			Debug.Log ("Has chocado el botón");
+			isTouched = true;
+		}
+	}
+
+	void OnCollisionExit(Collision collision)
+	{
+		if(collision.gameObject.name == "hand" && isTouched == true)
+		{
+			transform.localScale += new Vector3(0.1F, 0, 0); 
+			Debug.Log ("Has dejado de chocar el botón");
+			isTouched = false;
+		}
+	}
+		
+}
---
+++ Assets/Scripts/ScoreTextScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class ScoreTextScript : MonoBehaviour {
+
+	public GameObject itemlist;
+	public Text[] itemList;
+	public Text ganastePerdiste;
+	public Text winText;
+	public static int result = 2;
+
+	// Use this for initialization
+	void Start () {
+		SetGanastePerdisteText ();
+		displayItemList (itemlist);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void SetGanastePerdisteText ()
+	{
+		
+		if (result == 0) {
+			ganastePerdiste.text = "¡Ganaste!";
+
+		} else if (result == 1) {
+
+			ganastePerdiste.text = "¡Perdiste!";
+
+		} else if (result == 2) {
+
+			ganastePerdiste.fontSize = 20;
+			ganastePerdiste.text = "¡Se acabó el tiempo!";
+		}
+
+	}
+	void displayItemList(GameObject listOfItems){
+	
+		this.itemlist = listOfItems;
+		Debug.Log (itemList.ToString());
+	
+	}
+}
